72ad2ffad2ce6384724feb7868dde8114bf7f8b3
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-03-26 21:02:41 +0100

    Rendered player

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+var player rl.Texture2D
+
 func init() {
 	rl.InitWindow(1280, 720, "TileRPG")
+
+	player = rl.LoadTexture("./assets/graphics/player.png")
 }
 
 func update() {
@@ -13,6 +17,8 @@ func render() {
 	rl.BeginDrawing()
 
 	rl.ClearBackground(rl.Black)
+
+	rl.DrawTexture(player, 100, 100, rl.White)
 
 	rl.EndDrawing()
 }
