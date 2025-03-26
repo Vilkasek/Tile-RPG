11af7b080c908f015d935d9eb1f1937ce1a8344d
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-03-26 20:51:09 +0100

    Added initial window

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func init() {
+	rl.InitWindow(1280, 720, "TileRPG")
+}
+
+func update() {
+}
+
+func render() {
+	rl.BeginDrawing()
+
+	rl.ClearBackground(rl.RayWhite)
+
+	rl.EndDrawing()
+}
+
+func deinit() {
+	rl.CloseWindow()
+}
+
+func main() {
+	for !rl.WindowShouldClose() {
+		update()
+		render()
+	}
+
+	deinit()
+}
