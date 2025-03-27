b51163ae2191eb041b201d9470b0f52f308edb55
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-03-27 11:12:25 +0100

    Added simple ui logic

---
+++ ui/ui.go
@@ -0,0 +1,28 @@
+package ui
+
+import (
+	"tiled/utils"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type UI struct {
+	Texture  rl.Texture2D
+	Position rl.Vector2
+}
+
+func (u *UI) Init(path string, x int, y int) {
+	u.Texture = rl.LoadTexture(path)
+	u.Position = rl.Vector2{X: float32(x), Y: float32(y)}
+}
+
+func (u *UI) Deinit() {
+	rl.UnloadTexture(u.Texture)
+}
+
+func (u *UI) Update() {
+}
+
+func (u *UI) Render() {
+	rl.DrawTextureEx(u.Texture, u.Position, 0, utils.Get_Scale(), rl.White)
+}
